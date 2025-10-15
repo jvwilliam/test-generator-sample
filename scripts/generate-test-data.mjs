6a1acb5feff14ebeae3596eec4d8a5796d371744
jv.macbook <44486596+willjandal@users.noreply.github.com>
2025-10-15 22:24:47 +0400

    add files

---
+++ scripts/generate-test-data.mjs
@@ -0,0 +1,68 @@
+import ora from "ora";
+import chalk from "chalk";
+
+(   async () => {
+
+    async function openBrowser() {
+        const spinner = ora(chalk.yellow('Opening the browser instance...')).start();
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        spinner.succeed(chalk.green('Instance Successfuly Created!'));
+    };
+
+    async function loginWithUserCredentials() {
+        const spinner = ora(chalk.yellow('Logging in as Admin')).start();
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        spinner.succeed(chalk.green('Login Successful!'));
+    };
+
+    async function createParentCategory() {
+        const spinner = ora(chalk.yellow('Creating the Parent Category...')).start();
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        spinner.succeed(chalk.green('Parent Category Successfully Created!'));
+    };
+
+    async function createSubCategory() {
+        const spinner = ora(chalk.yellow('Creating the Sub Category...')).start();
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        spinner.succeed(chalk.green('Sub Category Successfuly Created!'));
+    };
+
+    async function createTestProduct() {
+        const spinner = ora(chalk.yellow('Creating the Test Product with ID TEST-001')).start();
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        await assignParentCategory();
+        await assignSubCategory();
+        await assignRelatedProducts();
+        spinner.succeed(chalk.green('Test Product Successfuly Created! Closing the browser instance.'));
+    };
+
+    async function assignParentCategory() {
+        const spinner = ora(chalk.yellow('Assigning Parent Categeory to Test Product with ID TEST-001')).start();
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        spinner.succeed(chalk.green('Parent Category Succesfully Assigned!'));
+    };
+
+    async function assignSubCategory() {
+        const spinner = ora(chalk.yellow('Assigning Sub Categeory to Test Product with ID TEST-001')).start();
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        spinner.succeed(chalk.green('Sub Category Succesfully Assigned!'));
+    };
+
+    async function assignRelatedProducts() {
+        const spinner = ora(chalk.yellow('Assigning Related Products to Test Product with ID TEST-001')).start();
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        spinner.succeed(chalk.green('Related Product Succesfully Assigned!!'));
+    };
+
+    async function generateTestDataSet() {
+        await openBrowser();
+        await loginWithUserCredentials();
+        await createParentCategory();
+        await createSubCategory();
+        await createTestProduct();
+    };
+
+    await generateTestDataSet();
+
+    
+})();
